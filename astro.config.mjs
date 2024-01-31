2d72f94bb2eac3b1cfc595d71ec7ac6f8b85f617
junior-boop <juniorseppo3@gmail.com>
2024-01-31 06:35:45 +0100

    server

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "hybrid",
+  output: "server",
   site: 'https://example.com',
   integrations: [mdx(), sitemap(), react({
     experimentalReactChildren: true
