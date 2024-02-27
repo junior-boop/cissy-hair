e39fc94288c2b9d579c660a6e3b3769b8398e8d7
junior-boop <juniorseppo3@gmail.com>
2024-02-27 14:11:22 +0100

    server

--- src/components/filtre.jsx
+++ src/components/filtre.jsx
@@ -9,11 +9,11 @@ export default function FilterComponent({passing_url}){
     return(
        <div className="my-8 overflow-x-auto overflow-y-hidden lg:overflow-x-hidden lg:overflow-y-hidden py-4">
                 <div className="flex gap-2 w-max px-4 lg:px-0">
-                <Categorie categorie='Mèches' href = '#meches' url = {passing_url} />
-                <Categorie categorie='Soins de peau' href = '#peau' url = {passing_url}/>
-                <Categorie categorie='Produits Capillaires' href = '#capillaires' url = {passing_url}/>
-                <Categorie categorie='Compléments Alimentaires' href = '#aliments' url = {passing_url} />
-                <Categorie categorie='Accessoires' href = '#accessoires' url = {passing_url} />
+                <Categorie categorie='Mèches' href = '#meches'  />
+                <Categorie categorie='Soins de peau' href = '#peau'/>
+                <Categorie categorie='Produits Capillaires' href = '#capillaires'/>
+                <Categorie categorie='Compléments Alimentaires' href = '#aliments' />
+                <Categorie categorie='Accessoires' href = '#accessoires' />
             </div>
        </div>
     )
